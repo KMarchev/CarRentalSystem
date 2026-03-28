485e71a59d7c3e37e6bbab5d4b270e395bdde6e5
KMarchev <kaloyan713@gmail.com>
2026-03-28 15:05:10 +0200

    CarRentalSystem 0.1.0
    feat: set up project package structure

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,17 @@
+package application;
+
+/**
+ * Entry point of the program.
+ */
+
+public class Application {
+    /**
+     * Main method that starts the "Car Rental System" program.
+     *
+     * @param args command-line arguments(not used).
+     */
+
+    public static void main(String[] args){
+        Program.run();
+    }
+}
